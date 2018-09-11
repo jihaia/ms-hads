68d08c10dce2fc8dfd0e7a99012729a09667f415
Jason Ihaia <jihaia@Jasons-MacBook-Pro.local>
2018-09-11 12:49:31 -0600

    minor update

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require('express');
 const app = express();
 
 app.get('/api', (req, res) => {
-  res.send({hello: 'motto'});
+  res.send({hello: process.env.sample});
 });
 
 app.post('/api', (req, res) => {
